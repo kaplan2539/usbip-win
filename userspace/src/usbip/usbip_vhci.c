ae0965fb3ffae1f387286ea7ea8a9924ac482477
Alexandre Demers <alexandre.f.demers@gmail.com>
2019-04-19 01:35:28 -0400

    Improve error and information messages
    Better identify messages' origin.
    
    Signed-off-by: Alexandre Demers <alexandre.f.demers@gmail.com>

--- userspace/src/usbip/usbip_vhci.c
+++ userspace/src/usbip/usbip_vhci.c
@@ -18,7 +18,7 @@ walker_devpath(HDEVINFO dev_info, PSP_DEVINFO_DATA pdev_info_data, devno_t devno
 
 	id_hw = get_id_hw(dev_info, pdev_info_data);
 	if (id_hw == NULL || strcmp(id_hw, "root\\usbip_vhci") != 0) {
-		err("invalid hw id: %s\n", id_hw ? id_hw : "");
+		err("%s: invalid hw id: %s\n", __FUNCTION__, id_hw ? id_hw : "");
 		if (id_hw != NULL)
 			free(id_hw);
 		return 0;
@@ -119,10 +119,14 @@ usbip_vhci_attach_device(HANDLE hdev, int port, usbip_wudev_t *wudev)
 
 	plugin.port = port;
 
-	if (DeviceIoControl(hdev, IOCTL_USBIP_VHCI_PLUGIN_HARDWARE,
+	if (!DeviceIoControl(vhci_driver->hc_device, IOCTL_USBIP_VHCI_PLUGIN_HARDWARE,
 		&plugin, sizeof(plugin), NULL, 0, &unused, NULL))
-		return 0;
-	return -1;
+	{
+		err("usbip_vhci_attach_device: DeviceIoControl failed: err: 0x%lx", GetLastError());
+		return -1;
+	}
+
+	return 0;
 }
 
 int
