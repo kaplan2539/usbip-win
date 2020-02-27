743e95b346dd252e007fb70ad82dc715abe0f383
kaplan2539 <kaplan2539@gmail.com>
2020-02-27 10:04:02 +0100

    Update wdk-fix.ps1

--- wdk-fix.ps1
+++ wdk-fix.ps1
@@ -1,16 +1,14 @@
-echo "HELLO"
-
-#try
-#{
-#     $process = Start-Process `
-#    -FilePath "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\Common7\IDE\VSIXInstaller.exe" `
-#    -ArgumentList ("/quiet", '"C:\Program Files (x86)\Windows Kits\10\Vsix\VS2019\WDK.vsix"') `
-#    -Wait `
-#    -PassThru
-#}
-#catch
-#{
-#    Write-Host "There is an error during WDK.vsix installation"
-#    $_
-#    exit 1
-#}
+try
+{
+     $process = Start-Process `
+    -FilePath "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\Common7\IDE\VSIXInstaller.exe" `
+    -ArgumentList ("/quiet", '"C:\Program Files (x86)\Windows Kits\10\Vsix\VS2019\WDK.vsix"') `
+    -Wait `
+    -PassThru
+}
+catch
+{
+    Write-Host "There is an error during WDK.vsix installation"
+    $_
+    exit 1
+}
